8160683f5ae1db41e1bbcd9744a88a76c5f59a11
Uuu897 <Nastiushakova@yandex.ru>
2024-02-17 03:53:49 +0700

    методы, вторая колонка

--- src/main.js
+++ src/main.js
@@ -21,4 +21,25 @@ new Vue({
             completedTasks: [],
         }
     },
+    methods:{
+        addTask() {
+            if (!this.newTask.priority) {
+                alert('Укажите приоритет задачи.');
+                return;
+            }
+            if (!this.newTask.title) {
+                alert('Укажите заголовок задачи.');
+                return;
+            }
+            if (!this.newTask.deadline) {
+                alert('Укажите дэдлайн.');
+                return;
+            }
+            if (new Date(this.newTask.deadline) <= new Date(this.newTask.createdAt)) {
+                alert('Дэдлайн не может быть раньше даты создания или равен ей.');
+                return;
+            }
+            this.plannedTasks.push({...this.newTask});
+            this.newTask = { title: '', description: '', deadline: '', createdAt: new Date().toLocaleString(), lastEdited: null };
+        },
 })
